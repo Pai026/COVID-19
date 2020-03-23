6ba4885c24b18a116587d77eeb21c70a7a4e970d
Abhiram Pai <abhirampai1999@gmail.com>
2020-03-23 08:18:35 +0530

    Add files via upload

--- extractData/extract3.py
+++ extractData/extract3.py
@@ -7,7 +7,7 @@ from folium.plugins import HeatMap, MarkerCluster
 import math
 import json
 
-data = pd.read_csv('../Data/coronadata.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-22-2020.csv')
 print(data['Province/State'])
 #Country/Region,Province/State,Confirmed,Deaths,Recovered,Latitude,Longitude
 m_3 = folium.Map(location=[20.5937,78.9629], tiles='CartoDB dark_matter', zoom_start=2)
@@ -15,7 +15,7 @@ m_3 = folium.Map(location=[20.5937,78.9629], tiles='CartoDB dark_matter', zoom_s
 fg=folium.FeatureGroup(name='Marker', show=True)
 m_3.add_child(fg)
 fg1=folium.FeatureGroup(name='Circle', show=False)
-m_3.add_child(fg1)
+#m_3.add_child(fg1)
 mc = MarkerCluster().add_to(fg)
 mc1=MarkerCluster().add_to(fg1)
 for idx, row in data.iterrows():
@@ -23,13 +23,13 @@ for idx, row in data.iterrows():
             location=[row['Latitude'],row['Longitude']]
             popup='<div style="width:300">'+'<STRONG>'+'<h3>'+str(row['Province/State'])+'</h3>'+'<br>'+'<p>'+'Confirmed-'+str(row['Confirmed'])+'<br>'+'Deaths-'+str(row['Deaths'])+'<br>'+'Recovered-'+str(row['Recovered'])+'</p>'+'</STRONG>'+'</div>'
             mc.add_child(Marker(location=location,popup=popup))
-            radius=15000 + int(row['Confirmed']) * 2500
+            radius=15000 + int(row['Confirmed']) * 20
             mc1.add_child(Circle(location=location,popup=popup,radius=radius,fill_colour='#FF0000',fill=True))
     else:
         location=[row['Latitude'],row['Longitude']]
         popup='<div style="width:300">'+'<STRONG>'+'<h3>'+row['Country/Region']+'</h3>'+'<br>'+'<p>'+'Confirmed-'+str(row['Confirmed'])+'<br>'+'Deaths-'+str(row['Deaths'])+'<br>'+'Recovered-'+str(row['Recovered'])+'</p>'+'</STRONG>'+'</div>'
         mc.add_child(Marker(location=location,popup=popup))    
-        radius=15000 + int(row['Confirmed']) * 2500
+        radius=15000 + int(row['Confirmed']) * 20
         mc1.add_child(Circle(location=location,popup=popup,radius=radius,fill_colour='#FF0000',fill=True))
 
 folium.LayerControl().add_to(m_3)
